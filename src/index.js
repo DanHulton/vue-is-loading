4278a20f68ee71c97784753858b4020fc29cac34
Dan Hulton <dan@danhulton.com>
2021-10-16 22:08:48 -0400

    0.0.1 development version.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/**
+ * Install the plugin.
+ *
+ * @param {Vue} app - The Vue app to install into.
+ */
+const install = (app) => {
+  console.log('vue-is-loading is installed!');
+};
+
+export {
+  install,
+  loadable,
+  mapLoadableMethods,
+};
